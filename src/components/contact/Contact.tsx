a3e0576eab54557ec1b5b7fd604335610d221bd1
Mayra Rincones <mayra.rinconess@gmail.com>
2024-06-09 20:25:47 +0200

    added preview image

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -17,12 +17,12 @@ export default function Contact({ contactRef }: ContactProps) {
       <Fade cascade>
         <p className="text-primary md:text-lg max-w-[500px]">
           ¡Reserva tu escapada perfecta hoy mismo y descubre todo lo que nuestro
-          encantador departamento tiene para ofrecer!
+          encantador apartamento tiene para ofrecer!
         </p>
         <p className="text-primary md:text-lg">
           No esperes más y contáctanos{" "}
           <a
-            href="https://wa.me/+584168840028?text=Hola%2C%20buenos%20d%C3%ADas%20%3AD%0AQuisiera%20hacer%20una%20reserva%20en%20su%20departamento.%20%C2%BFC%C3%B3mo%20puedo%20hacer%3F"
+            href="https://wa.me/+584168840028?text=Hola%2C%20buenos%20d%C3%ADas%20%3AD%0AQuisiera%20hacer%20una%20reserva%20en%20su%20apartamento.%20%C2%BFC%C3%B3mo%20puedo%20hacer%3F"
             target="_blank"
             className="inline-block underline hover:transition-all hover:text-light hover:scale-125 hover:-translate-y-2"
           >
